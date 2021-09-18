97a7986acf0b6f67dbc0aa6813238376002ea989
Tharuk Renuja <90763454+TharukRenuja@users.noreply.github.com>
2021-09-18 09:38:17 +0530

    Update __init__.py

--- nstcentertainmentbot/helpers/database/__init__.py
+++ nstcentertainmentbot/helpers/database/__init__.py
@@ -21,7 +21,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+    engine = create_engine(os.getenv("DB_URI"))
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
