529895e87ab402ee23d4224b0b023c68806b49aa
Tharuk Renuja <90763454+TharukRenuja@users.noreply.github.com>
2021-09-18 09:08:45 +0530

    Add files via upload

---
+++ nstcentertainmentbot/config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config(object):
+    TOKEN = os.environ.get("TOKEN")
+    WORKERS = int(os.environ.get("WORKERS", 8))
+    TMDBAPI = os.environ.get("TMDBAPI")
+    DB_URI = os.environ.get("DATABASE_URL")
+    GENIUS = os.environ.get("GENIUS")
+    SPT_CLIENT_SECRET = os.environ.get("SPT_CLIENT_SECRET")
+    SPT_CLIENT_ID = os.environ.get("SPT_CLIENT_ID")
+    DEBUG = bool(os.environ.get("DEBUG", False))
+    ARLTOKEN = os.environ.get("ARL")
+    APP_URL = os.environ.get("APP_URL")
+    APIID = os.environ.get("APIID")
+    APIHASH = os.environ.get("APIHASH")
