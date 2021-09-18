fdf20023e8f35b5960df47b29a23580eb9cff723
Tharuk Renuja <90763454+TharukRenuja@users.noreply.github.com>
2021-09-18 09:54:16 +0530

    Update __init__.py

--- nstcentertainmentbot/helpers/database/__init__.py
+++ nstcentertainmentbot/helpers/database/__init__.py
@@ -16,12 +16,13 @@
 import os
 import sys
 from nstcentertainmentbot import DB_URI
-
+from flask import Flask, render_template, request
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 def start() -> scoped_session:
+    DB_URI = 'postgres://bkwjpcnnwxaafe:de2682d5d15bb4f2d4e2878a5edc7adbcb8fc92d21dfc3d913d4923439f9def0@ec2-3-219-111-26.compute-1.amazonaws.com:5432/dctnghfb4ns1kn'
     engine = create_engine(DB_URI, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
